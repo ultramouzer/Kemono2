61d5090319099a1a26960a720edf30ecdd2eaa6e
Anonymous <>
2020-12-05 08:55:14 -0500

    Fix thumbnails

--- static/js/utils.js
+++ static/js/utils.js
@@ -30,7 +30,7 @@ function thumbHTML (data) {
     <a href="${data.href}" class="thumb-link">
       ${data.src ? `
         <div class="thumb thumb-with-image ${data.class || 'thumb-standard'}">
-          <img src="https://images.weserv.nl/?url=https://kemono.party${data.src.replace('https://kemono.party', '')}&width=500&output=jpg&we">
+          <img src="https://images.weserv.nl/?url=https://kemono.party${data.src.replace('https://kemono.party', '')}&width=500&output=jpg">
         </div>
       ` : `
         <div class="thumb thumb-with-text ${data.class || 'thumb-standard'}">
