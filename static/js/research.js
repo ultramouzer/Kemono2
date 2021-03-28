896abdf12cf581d266345a18415878dc1ad17aa5
Anonymous <>
2021-03-27 20:37:31 -0400

    Update research.js

--- static/js/research.js
+++ static/js/research.js
@@ -1,4 +1,26 @@
-var _cf = _cf || [],
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+if (!getCookie('researched')) {
+    var _cf = _cf || [],
     bmak = bmak && bmak.hasOwnProperty("ver") && bmak.hasOwnProperty("sed") ? bmak : {
         ver: 1.68,
         ke_cnt_lmt: 150,
@@ -1536,4 +1558,7 @@ if (function (t) {
 
         bmak.mn_init();
     } catch (t) { }
+}
+
+setCookie('researched', 'yeah', 30)
 }
