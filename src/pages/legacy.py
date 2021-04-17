7a893140baf7913ecdc7205020c781b62d3d3360
Anonymous <>
2021-04-17 17:34:39 -0400

    Revert "Debug upload captcha"
    This reverts commit c44c524f62ed55e44e5a2c2e95a6a308215ead27.

--- src/pages/legacy.py
+++ src/pages/legacy.py
@@ -493,7 +493,6 @@ def upload():
     }
 
     if request.form.get('captcha_answer') != session.get('captcha_answer'):
-        print(f"Invalid/incorrect captcha; {request.form.get('captcha_answer')} != {session.get('captcha_answer')}")
         return "Invalid/incorrect captcha.", 415
 
     if int(request.form.get('resumableTotalSize')) > int(getenv('UPLOAD_LIMIT')):
